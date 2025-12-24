9ae9b980275b004eff788455c68fb07462cc186b
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-24 18:29:21 +0800

    Implement UpSave class as BankAccountDecorator

--- src/UpSave.java
+++ src/UpSave.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class UpSave implements BankAccountDecorator {
+    private BankAccountDecorator bankAccount;
 
-public class UpSave {
+    public UpSave(BankAccountDecorator bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showInfo() {
+        return bankAccount.showInfo();
+    }
+
+    @Override
+    public String showAccountType() {
+        return "UpSave";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.04;
+    }
+
+    @Override
+    public double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showBenefits() {
+        return bankAccount.showBenefits() + ", With Insurance";
+    }
 }
