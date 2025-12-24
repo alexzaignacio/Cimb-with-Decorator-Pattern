4557b8433d9c170950542e4ad88d6e2f90ee77b4
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-24 18:28:38 +0800

    Implement SavingsAccount class with BankAccountDecorator

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+public class SavingsAccount implements BankAccountDecorator {
+    private int accountNumber;
+    private String accountName;
+    private double balance;
 
-public class SavingsAccount {
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String showInfo() {
+        return "Account number: " + accountNumber + " Account name: " + accountName + " Balance: " + balance;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "Savings Account";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.01;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showBenefits() {
+        return "Standard Savings Account";
+    }
 }
