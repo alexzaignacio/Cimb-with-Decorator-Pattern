3218f8a3216b6aad70d58a12045315baca990d6f
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-24 18:29:43 +0800

    Add GSave class to enhance bank account functionality
    Implemented GSave class as a decorator for BankAccountDecorator, adding interest rate and benefits.

--- src/GSave.java
+++ src/GSave.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class GSave implements BankAccountDecorator {
+    private BankAccountDecorator bankAccount;
 
-public class GSave {
+    public GSave(BankAccountDecorator bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showInfo() {
+        return bankAccount.showInfo();
+    }
+
+    @Override
+    public String showAccountType() {
+        return "GSave";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.025;
+    }
+
+    @Override
+    public double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showBenefits() {
+        return bankAccount.showBenefits() + ", GCash Transfer";
+    }
 }
